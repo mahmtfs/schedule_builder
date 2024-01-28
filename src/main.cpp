346eac2bd2dfe084dcdf35196004bf5f5a6663d9
mahmtfs <rinnik228@gmail.com>
2024-01-27 21:10:43 -0500

    Ready for Windows release

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,8 @@ static void glfw_error_callback(int error, const char* description)
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
 }
 
-int main(int, char**)
+
+int WinMain()
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
